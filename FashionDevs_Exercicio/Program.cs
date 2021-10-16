ae65d72e727d616e2be3f5af4606b1852012d92e
Neilyane Silva <neilyane.ssouza1@gmail.com>
2021-10-16 10:30:16 -0300

    Adicionar arquivos de projeto.

---
+++ FashionDevs_Exercicio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FashionDevs_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao;
+            double n1;
+            double n2;
+            double resultado;
+            bool validacao = true;
+            Program program = new Program();
+
+            Console.Write("C&A Fashion Devs");
+
+            do
+            {
+                Console.Write("Selecione a opção desejada");
+                Console.WriteLine("1. Cálculo de IMC");
+
+            }
+        }
+    }
+}
